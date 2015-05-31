aedf167ae6026ee1238f8f51e5eb4bcf6e3e040e
diegoarize <diegoarize@outlook.com>
2015-05-31 15:42:54 -0300

    Tree.java

---
+++ src/Tree.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+
+public class Tree {
+	public static LinkedList<Node> AST;
+	
+	public void printAST() {
+		//TODO: implementar forma de imprimir
+		//TODO: verificar se não é melhor implementar o print no NODE e cada objeto implementa sua forma de imprimir
+	}
+}
