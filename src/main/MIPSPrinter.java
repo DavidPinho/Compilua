7e7a1bb94cd49db6995ae40819d48328c98e5e08
diegoarize <diegoarize@outlook.com>
2015-11-19 17:06:00 -0300

    adicionando comentarios

--- src/main/MIPSPrinter.java
+++ src/main/MIPSPrinter.java
@@ -4,6 +4,13 @@ public class MIPSPrinter {
 	/* TODO: Usar o mesmo arquivo a ser impresso pra facilitar
 	 * Usar o metodo init para iniciar o arquivo e funcionar tudo que precise
 	 * e o finish pra fechar o arquivo.
+	 * 
+	 * TODO: usar uma estrutura talvez como um String[] pra armazenar o ccodigo gerado num buffer
+	 * ai no metodo finish varre o buffer e imprime no arquivo, fechando o mesmo sequencialmente.
+	 * 
+	 * A ideia do buffer eh pq no assembly tem o segmento .data pra gdeclaracao de variaveis
+	 * e isso pode ficar baguncado na geracao. sugiro o buffer do data e do text ai gravamos no arquivo o do
+	 * data e depois o do text, acho que fica mais facil.
 	 */
 
 	public static void init() {
